090751833446f2594df4a55e6a13dee0a75ba232
juanyepesp <j.yepes@uniandes.edu.co>
2025-05-25 12:37:51 -0500

    add michael notebooks

--- src/back/ml_classic.py
+++ src/back/ml_classic.py
@@ -10,6 +10,7 @@ MODEL_DIR = os.path.join(os.path.dirname(__file__), "models_ml")
 vectorizer = joblib.load(os.path.join(MODEL_DIR, "vectorizer.pkl"))
 logistic_model = joblib.load(os.path.join(MODEL_DIR, "logistic_regression_model.pkl"))
 naive_bayes_model = joblib.load(os.path.join(MODEL_DIR, "modelo_naivebayes.pkl"))
+svm_model = joblib.load(os.path.join(MODEL_DIR, "modelo_svm.pkl"))
 
 def predict_with_logistic_regression(text: str) -> dict:
     X = vectorizer.transform([text])
@@ -26,5 +27,15 @@ def predict_with_naive_bayes(text: str) -> dict:
     return {
         "etiqueta_aita": prediction,
         "razonamiento": "Predicción generada por Naive Bayes basada en texto vectorizado.",
+        "text": text
+    }
+
+def predict_with_svm(text: str) -> dict:
+    # TODO
+    X = vectorizer.transform([text])
+    prediction = svm_model.predict(X)[0]
+    return {
+        "etiqueta_aita": prediction,
+        "razonamiento": "Predicción generada por SVM basada en texto vectorizado.",
         "text": text
     }
